79999024184be3380ae77be59217e51a49e72e3b
lqiang2003cn <randycoq@163.com>
2024-02-04 11:03:34 +0800

    d

--- Chapter 3/1_Central_Tendency_Measures.py
+++ Chapter 3/1_Central_Tendency_Measures.py
@@ -57,8 +57,7 @@ print('The median of the dataset: ' + str(median), '%')
 plt.plot(cpi_latest[:], color='black', linewidth=1.5, label='Change in CPI Year-on-Year')
 
 # Plotting horizontal lines that represent the median and the zero threshold
-plt.axhline(y=median, color='red', linestyle='dashed',
-            label='Median')
+plt.axhline(y=median, color='red', linestyle='dashed', label='Median')
 plt.axhline(y=0, color='blue', linestyle='dashed', linewidth=1)
 
 # Calling a grid to facilitate the visual component

bee2102c940564ba5abcd03c929519a667a77b57
lqiang2003cn <randycoq@163.com>
2024-02-04 11:02:44 +0800

    d

--- Chapter 1/VIX_Analysis.py
+++ Chapter 1/VIX_Analysis.py
@@ -3,7 +3,7 @@ import pandas_datareader as pdr
 
 # Setting the beginning and end of the historical data
 start_date = '1990-01-01'
-end_date   = '2023-01-23'
+end_date = '2023-01-23'
 
 # Creating a dataframe and downloading the VIX data
 vix = pdr.DataReader('VIXCLS', 'fred', start_date, end_date)
@@ -21,10 +21,10 @@ print('Number of nan values in the VIX dataframe: ' + str(count_nan))
 vix = vix.dropna()
 
 # Taking the differences in an attempt to make the data stationary
-vix = vix.diff(periods = 1, axis = 0)
+vix = vix.diff(periods=1, axis=0)
 
 # Dropping the first value of the data frame
-vix = vix.iloc[1: , :]
+vix = vix.iloc[1:, :]
 
 # Calculating the mean of the dataset
 mean = vix["VIXCLS"].mean()
@@ -36,11 +36,10 @@ print('The mean of the dataset = ' + str(mean))
 import matplotlib.pyplot as plt
 
 # Plotting the latest 250 observations in black with a label
-plt.plot(vix[-250:], color = 'black', linewidth = 1.5, 
-         label = 'Change in VIX')
+plt.plot(vix[-250:], color='black', linewidth=1.5, label='Change in VIX')
 
 # Plotting a red dashed horizontal line that is equal to mean
-plt.axhline(y = mean, color = 'red', linestyle = 'dashed')
+plt.axhline(y=mean, color='red', linestyle='dashed')
 
 # Calling a grid to facilitate the visual component
 plt.grid()
--- Chapter 3/1_Central_Tendency_Measures.py
+++ Chapter 3/1_Central_Tendency_Measures.py
@@ -3,7 +3,7 @@ import pandas_datareader as pdr
 
 # Setting the beginning and end of the historical data
 start_date = '1950-01-01'
-end_date   = '2023-01-23'
+end_date = '2023-01-23'
 
 # Creating a dataframe and downloading the CPI data
 cpi = pdr.DataReader('CPIAUCSL', 'fred', start_date, end_date)
@@ -18,7 +18,7 @@ count_nan = cpi['CPIAUCSL'].isnull().sum()
 print('Number of nan values in the CPI dataframe: ' + str(count_nan))
 
 # Transforming the CPI into a year-on-year measure
-cpi = cpi.pct_change(periods = 12, axis = 0) * 100
+cpi = cpi.pct_change(periods=12, axis=0) * 100
 
 # Dropping the nan values from the rows
 cpi = cpi.dropna()
@@ -34,13 +34,11 @@ print('The mean of the dataset: ' + str(mean), '%')
 import matplotlib.pyplot as plt
 
 # Plotting the latest observations in black with a label
-plt.plot(cpi_latest[:], color = 'black', linewidth = 1.5, 
-         label = 'Change in CPI Year-on-Year')
+plt.plot(cpi_latest[:], color='black', linewidth=1.5, label='Change in CPI Year-on-Year')
 
 # Plotting horizontal lines that represent the mean and the zero threshold
-plt.axhline(y = mean, color = 'red', linestyle = 'dashed', 
-         label = 'Mean')
-plt.axhline(y = 0, color = 'blue', linestyle = 'dashed', linewidth = 1)
+plt.axhline(y=mean, color='red', linestyle='dashed', label='Mean')
+plt.axhline(y=0, color='blue', linestyle='dashed', linewidth=1)
 
 # Calling a grid to facilitate the visual component
 plt.grid()
@@ -50,33 +48,21 @@ plt.legend()
 plt.close()
 
 # Calculating the median of the dataset
-median = cpi_latest["CPIAUCSL"].median() 
+median = cpi_latest["CPIAUCSL"].median()
 
 # Printing the result
 print('The median of the dataset: ' + str(median), '%')
 
 # Plotting the latest observations in black with a label
-plt.plot(cpi_latest[:], color = 'black', linewidth = 1.5, 
-         label = 'Change in CPI Year-on-Year')
+plt.plot(cpi_latest[:], color='black', linewidth=1.5, label='Change in CPI Year-on-Year')
 
 # Plotting horizontal lines that represent the median and the zero threshold
-plt.axhline(y = median, color = 'red', linestyle = 'dashed', 
-            label = 'Median')
-plt.axhline(y = 0, color = 'blue', linestyle = 'dashed', linewidth = 1)
+plt.axhline(y=median, color='red', linestyle='dashed',
+            label='Median')
+plt.axhline(y=0, color='blue', linestyle='dashed', linewidth=1)
 
 # Calling a grid to facilitate the visual component
 plt.grid()
 
 # Calling the legend function so it appears with the chart
 plt.legend()
-
-
-
-
-
-
-
-
-
-
-
